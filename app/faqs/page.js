9c9bf70b968f1fc5324084a9f8842a012ab96f69
Todd Heckeler <todd@consumedesign.com>
2023-05-26 11:20:41 -0400

    css fix

--- app/faqs/page.js
+++ app/faqs/page.js
@@ -2,6 +2,8 @@ import CatSection1 from "./components/CatSection1";
 import CatSection2 from "./components/CatSection2";
 import CatSection3 from "./components/CatSection3";
 
+import "./scss/main.scss";
+
 export default function Page() {
 	return (
 		<div>
